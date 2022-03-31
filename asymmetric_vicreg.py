b0e13206b86ebb95fe5d71d89ea6c4985a20f450
jeanne-ber <jeanne.berthonneau@gmail.com>
2022-03-31 12:18:17 +0200

    Corrections sur le dataloader

--- asymmetric_vicreg.py
+++ asymmetric_vicreg.py
@@ -8,6 +8,7 @@ import math
 import os
 import sys
 import time
+import urllib
 
 
 import torch
@@ -52,25 +53,34 @@ def get_arguments():
     return parser
 
 
+
+
+
 def main(args):
+    
     torch.backends.cudnn.benchmark = True
     init_distributed_mode(args)
     print(args)
     gpu = torch.device(args.device)
 
+    
     if args.rank == 0:
         args.exp_dir.mkdir(parents=True, exist_ok=True)
+        
         stats_file = open(args.exp_dir / "stats.txt", "a", buffering=1)
         print(" ".join(sys.argv))
         print(" ".join(sys.argv), file=stats_file)
+        
         stats_test_file = open(args.exp_dir / "stats_test.txt", "a", buffering=1)
         print(" ".join(sys.argv))
         print(" ".join(sys.argv), file=stats_test_file)
 
+        
+
     transforms = aug.TrainTransform()
     datadir = "/data1/jeanne/datasets/dogs_cats"
-    
     dataset = datasets.ImageFolder(datadir, transforms)
+    
     num_data = len(dataset)
     indices = list(range(num_data))
     np.random.shuffle(indices)
@@ -79,35 +89,49 @@ def main(args):
     
     train_sampler = SubsetRandomSampler(train_idx)
     test_sampler = SubsetRandomSampler(test_idx)
-    assert args.batch_size % args.world_size == 0
     
+    assert args.batch_size % args.world_size == 0
     kwargs = dict(batch_size=args.batch_size // args.world_size, num_workers=args.num_workers, pin_memory=True)
     train_loader = torch.utils.data.DataLoader(dataset, sampler=train_sampler, **kwargs)
     test_loader = torch.utils.data.DataLoader(dataset, sampler=test_sampler, **kwargs)
+
     
     
     model = Asym_VICReg(args).cuda(gpu)
     model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    
     optimizer = LARS(model.parameters(), lr=0, weight_decay=args.wd, weight_decay_filter=exclude_bias_and_norm, lars_adaptation_filter=exclude_bias_and_norm)
 
+
+    
     if (args.exp_dir / "model.pth").is_file():
         if args.rank == 0:
             print("resuming from checkpoint")
+            
         ckpt = torch.load(args.exp_dir / "model.pth", map_location="cpu")
         start_epoch = ckpt["epoch"]
         model.load_state_dict(ckpt["model"])
         optimizer.load_state_dict(ckpt["optimizer"])
+
+        
     else:
         start_epoch = 0
 
-    sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
+
+        
+
+    #sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
     
     start_time = last_logging = time.time()
     scaler = torch.cuda.amp.GradScaler()
+    
     for epoch in range(start_epoch, args.epochs):
-        sampler.set_epoch(epoch)
+        
+        train_sampler.set_epoch(epoch)
+        
         for step, ((x, y), _) in enumerate(train_loader, start=epoch * len(train_loader)):
+            
             x = x.cuda(gpu, non_blocking=True)
             y = y.cuda(gpu, non_blocking=True)
 
@@ -120,21 +144,32 @@ def main(args):
             scaler.step(optimizer)
             scaler.update()
 
+            
             current_time = time.time()
             if args.rank == 0 and current_time - last_logging > args.log_freq_time:
+                
                 stats = dict(epoch=epoch, step=step, loss=loss.item(), time=int(current_time - start_time), lr=lr)
                 print(json.dumps(stats))
                 print(json.dumps(stats), file=stats_file)
                 last_logging = current_time
 
+
+                
         if args.rank == 0:
             state = dict(epoch=epoch + 1, model=model.state_dict(), optimizer=optimizer.state_dict())
             torch.save(state, args.exp_dir / "model.pth")
 
+
+            
     if args.rank == 0:
         torch.save(model.module.backbone_1.state_dict(), args.exp_dir / "resnet50.pth")
         torch.save(model.module.backbone_2.state_dict(), args.exp_dir / "resnet50x2.pth")
 
+
+
+
+
+        
     backbone, embedding = resnet.__dict__["resnet50"](zero_init_residual=True)
     state_dict_test = torch.load(args.exp_dir / "resnet50.pth")
     missing_keys, unexpected_keys = backbone.load_state_dict(state_dict_test, strict=False)
@@ -143,18 +178,24 @@ def main(args):
     head = nn.Linear(embedding, 1000)
     head.weight.data.normal_(mean=0.0, std=0.01)
     head.bias.data.zero_()
+    
     model_test = nn.Sequential(backbone, head)
     model_test.cuda(gpu)
+    
     backbone.requires_grad_(False)
     head.requires_grad_(True)
     model_test = torch.nn.parallel.DistributedDataParallel(model_test, device_ids=[gpu])
 
+    
+
     criterion = nn.CrossEntropyLoss().cuda(gpu)
 
-    param_groups = [dict(params=head.parameters(), lr=args.lr_head)]
-    optimizer = optim.SGD(param_groups, 0, momentum=0.9, weight_decay=args.weight_decay)
+    param_groups = [dict(params=head.parameters(), lr=0.02)]
+    optimizer = optim.SGD(param_groups, 0, momentum=0.9, weight_decay=args.wd)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
 
+
+    
     if (args.exp_dir / "checkpoint.pth").is_file():
         ckpt = torch.load(args.exp_dir / "checkpoint.pth", map_location="cpu")
         start_epoch = ckpt["epoch"]
@@ -162,69 +203,114 @@ def main(args):
         model_test.load_state_dict(ckpt["model"])
         optimizer.load_state_dict(ckpt["optimizer"])
         scheduler.load_state_dict(ckpt["scheduler"])
+        
     else:
         start_epoch = 0
         best_acc = argparse.Namespace(top1=0, top5=0)
 
 
+
+        
+    start_epoch_test = 0
     start_time_test = time.time()
+    
     for epoch in range(start_epoch_test, args.epochs_test):
 
         model.eval()
+        
         if args.rank == 0:
+            
             top1 = AverageMeter("Acc@1")
             top5 = AverageMeter("Acc@5")
+            
             with torch.no_grad():
                 for images, target in test_loader:
-                    output = test_model(images.cuda(gpu, non_blocking=True))
+                    print(len(images))
+                    output = model_test(images.cuda(gpu, non_blocking=True))
+                    
                     acc1, acc5 = accuracy(output, target.cuda(gpu, non_blocking=True), topk=(1, 5))
                     top1.update(acc1[0].item(), images.size(0))
                     top5.update(acc5[0].item(), images.size(0))
+
+                    
             best_acc.top1 = max(best_acc.top1, top1.avg)
             best_acc.top5 = max(best_acc.top5, top5.avg)
+            
             stats = dict(epoch=epoch, acc1=top1.avg, acc5=top5.avg, best_acc1=best_acc.top1, best_acc5=best_acc.top5)
             print(json.dumps(stats))
             print(json.dumps(stats), file=stats_test_file)
 
+            
         scheduler.step()
+        
         if args.rank == 0:
             state = dict(epoch=epoch + 1, best_acc=best_acc, model=model.state_dict(), optimizer=optimizer.state_dict(), scheduler=scheduler.state_dict())
             torch.save(state, args.exp_dir / "checkpoint.pth")
 
+
+
+
+
+            
 def adjust_learning_rate(args, optimizer, loader, step):
+    
     max_steps = args.epochs * len(loader)
     warmup_steps = 10 * len(loader)
     base_lr = args.base_lr * args.batch_size / 256
+
+    
     if step < warmup_steps:
         lr = base_lr * step / warmup_steps
+        
     else:
         step -= warmup_steps
         max_steps -= warmup_steps
         q = 0.5 * (1 + math.cos(math.pi * step / max_steps))
         end_lr = base_lr * 0.001
         lr = base_lr * q + end_lr * (1 - q)
+
+        
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
+
+        
     return lr
 
 
+
+
+
 class Asym_VICReg(nn.Module):
+    
     def __init__(self, args):
+        
         super().__init__()
         self.args = args
         self.num_features = int(args.mlp.split("-")[-1])
+        
+        
         if args.arch_1 == "resnetencoder":
             self.backbone_1, self.embedding_1 = ResnetEncoder()
+            
         else:
             self.backbone_1, self.embedding_1 = resnet.__dict__[args.arch_1](zero_init_residual=True)
+
+            
         if args.arch_2 =="resnetencoder":
             self.backbone_2, self.embedding_2 = ResnetEncoder()
+            
         else:
             self.backbone_2, self.embedding_2 = resnet.__dict__[args.arch_2](zero_init_residual=True)
+
+            
         self.projector_1 = Projector(args, self.embedding_1)
         self.projector_2 = Projector(args, self.embedding_2)
 
+
+
+        
     def forward(self, x, y):
+        
         x = self.projector_1(self.backbone_1(x))
         y = self.projector_2(self.backbone_2(y))
 
@@ -244,41 +330,64 @@ class Asym_VICReg(nn.Module):
         cov_loss = off_diagonal(cov_x).pow_(2).sum().div(self.num_features) + off_diagonal(cov_y).pow_(2).sum().div(self.num_features)
 
         loss = (self.args.sim_coeff * repr_loss + self.args.std_coeff * std_loss + self.args.cov_coeff * cov_loss)
+
+        
         return loss
 
+    
 
 def Projector(args, embedding):
+    
     mlp_spec = f"{embedding}-{args.mlp}"
     layers = []
     f = list(map(int, mlp_spec.split("-")))
+    
     for i in range(len(f) - 2):
         layers.append(nn.Linear(f[i], f[i + 1]))
         layers.append(nn.BatchNorm1d(f[i+1]))
         layers.append(nn.ReLU(True))
+        
     layers.append(nn.Linear(f[-2], f[-1], bias=False))
+
+    
     return nn.Sequential(*layers)
+
+
+
 
 
 def exclude_bias_and_norm(p):
     return p.ndim == 1
 
 
+
+
 def off_diagonal(x):
     n,m = x.shape
     assert n == m
+    
     return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
 
 
 
+
+
+
 class LARS(optim.Optimizer):
+
     def __init__(self, params, lr, weight_decay=0, momentum=0.9, eta=0.001, weight_decay_filter=None, lars_adaptation_filter=None):
+        
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, eta=eta, weight_decay_filter=weight_decay_filter, lars_adaptation_filter=lars_adaptation_filter)
         super().__init__(params, defaults)
 
+
+        
     @torch.no_grad()
     def step(self):
+        
         for g in self.param_groups:
             for p in g["params"]:
+                
                 dp = p.grad
 
                 if dp is None:
@@ -295,32 +404,80 @@ class LARS(optim.Optimizer):
                     dp = dp.mul(q)
 
                 param_state = self.state[p]
+                
                 if "mu" not in param_state:
                     param_state["mu"] = torch.zeros_like(p)
+                    
                 mu = param_state["mu"]
                 mu.mul_(g["momentum"]).add_(dp)
 
                 p.add_(mu, alpha=-g["lr"])
 
 
+
+                
+
 def batch_all_gather(x):
     x_list = FullGatherLayer.apply(x)
+    
     return torch.cat(x_list, dim=0)
+
+
+
 
 
 class FullGatherLayer(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, x):
+        
         output = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
         dist.all_gather(output, x)
+        
         return tuple(output)
 
+
+    
     @staticmethod
     def backward(ctx, *grads):
+        
         all_gradients = torch.stack(grads)
         dist.all_reduce(all_gradients)
+        
         return all_gradients[dist.get_rank()]
+
+
+    
+
+    
+    
+class AverageMeter(object):
+    
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+        
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+        
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val*n
+        self.count += n
+        self.avg = self.sum / self.count
+
+        
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Asym_VICReg training script', parents=[get_arguments()])
