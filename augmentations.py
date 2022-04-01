919d489ea5cb46f966aeec4adb8e92aaa86c096b
jeanne-ber <jeanne.berthonneau@gmail.com>
2022-04-01 17:55:31 +0200

    Ajout d'un masking

--- augmentations.py
+++ augmentations.py
@@ -9,7 +9,9 @@ from PIL import ImageOps, ImageFilter
 import numpy as np
 import torchvision.transforms as transforms
 from torchvision.transforms.functional import InterpolationMode
-
+import torch.nn as nn
+import torch
+from timm.models.vision_transformer import PatchEmbed
 
 class GaussianBlur(object):
     def __init__(self, p):
@@ -87,3 +89,32 @@ class TrainTransform(object):
         x1 = self.transform(sample)
         x2 = self.transform_prime(sample)
         return x1, x2
+
+
+class Masking(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img, mask_ratio):
+
+        x  = PatchEmbed(img_size'????????', patch_size, in_chans, embed_dim).forward(img)  #arguments à définir
+        num_patches = PatchEmbed(img_size, patch_size, in_chans, embed_dim).num_patches
+        x = x + nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False)[:, 1:, :]
+        
+        N, L, D = x.shape
+        len_keep = int(L * (1 - mask_ratio))
+
+        noise = torch.rand(N, L, device=img.device)
+
+        ids_shuffle = torch.argsort(noise, dim=1)
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        mask = torch.ones([N, L], device=img.device)
+        mask[:, :len_keep] = 0
+
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+        
+        return x_masked, mask, ids_restore
