192951e0629c7676c0beb63b147a6e7b06fc241a
jeanne-ber <jeanne.berthonneau@gmail.com>
2022-04-11 14:33:48 +0200

    Tests de vibcreg avec le lightweighted resnet

--- main_vibcreg.py
+++ main_vibcreg.py
@@ -23,6 +23,7 @@ import numpy.typing
 from imgaug.augmenters import arithmetic
 
 import resnet
+import lightweighted_resnet
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -213,9 +214,11 @@ class VIbCReg(nn.Module):
         super().__init__()
         self.args = args
         self.num_features = int(args.mlp.split("-")[-1])
-        self.backbone, self.embedding = resnet.__dict__[args.arch](
-            zero_init_residual=True
-            )
+        #self.backbone, self.embedding = resnet.__dict__[args.arch](
+        #    zero_init_residual=True
+        #    )
+        self.backbone = lightweighted_resnet.light_resnet(zero_init_residual=True)
+        self.embedding = 1000
         self.projector = Projector(args, self.embedding)
 
 
